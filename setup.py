09c3b8db25eba91d57aa36360ed3a235952e4b8b
Tamas Norbert Varga <vargatn@gmail.com>
2021-10-13 15:41:51 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import sys
+import os
+from setuptools import setup, find_packages
+
+
+# if sys.version_info[0] != 3:
+#     sys.exit("Only python 3 is supported at the moment")
+
+setup(name="skysampler",
+      packages=find_packages(),
+      description="Generate random realizations of line-of-sights in optical sky surveys ",
+      #install_requires=['numpy', 'scipy', 'pandas', 'astropy', 'healpy', 'fitsio', 'sklearn'],
+      author="Tamas Norbert Varga",
+      author_email="T.Varga@physik.lmu.de",
+      version="0.3")
+
+# TODO this might interfere with NERSC deployment
+# create user project file
+home_path = os.path.expanduser("~")
+project_path = os.getcwd()
+with open(home_path + "/.skysampler.yaml", "w+") as file:
+    message = "project_path: " + project_path + "/"
+    file.write(message)
+
+# TODO Add an automatic path file generation here
+default_path = project_path + "/default_path.yaml"
+custom_path = project_path + "/settings/paths.yaml"
+
+
+
+
