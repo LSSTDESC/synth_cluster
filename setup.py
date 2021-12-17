68dc02430ea28878bac8a03c5dae3a6af10eb306
Tamas Norbert Varga <vargatn@gmail.com>
2021-12-17 15:28:12 +0100

    fixed package install name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 # if sys.version_info[0] != 3:
 #     sys.exit("Only python 3 is supported at the moment")
 
-setup(name="skysampler-lsst-lsst",
+setup(name="skysampler-lsst",
       packages=find_packages(),
       description="Generate random realizations of line-of-sights in optical sky surveys ",
       #install_requires=['numpy', 'scipy', 'pandas', 'astropy', 'healpy', 'fitsio', 'sklearn'],
